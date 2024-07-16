b12ad472d0d0e215819219355f2e4fc669e34b05
JoacoCavarzan <88760680+JoacoCavarzan@users.noreply.github.com>
2024-07-16 20:13:35 -0300

    Update Modbus

--- clienteModbus.py
+++ clienteModbus.py
@@ -1,12 +1,30 @@
+"""
+    La lámpara enciende y apaga de forma alternada, mientras no finalice el programa
+
+"""
 from pymodbus.client import ModbusTcpClient
+import time
 
 cliente = ModbusTcpClient("192.168.5.20")
 
+# Comandos posibles:
+# Lectura ---
+# cliente.read_coils(direccion,cantidad)
+# cliente.read_discrete_inputs(direccion,cantidad)
+# cliente.read_input_registers(direccion,cantidad)
+# cliente.read_holding_registers(direccion,cantidad)
+# Escritura ---
+# cliente.write_coil(direccion,valor)
+# cliente.write_register(direccion,cantidad)
+
 cliente.connect()
 
-#cliente.write_coll(1, True)
+try:
+    while True:
+        cliente.write_coll(1, True)
+        time.sleep(0.5)
+        cliente.write_coll(1, False)
+        time.sleep(0.5)
 
-lectura = cliente.read_discrete_inputs(0,1)
-print("El estado de entrada es", lectura.bits[0])
-
+except KeyboardInterrupt:
 cliente.close()
