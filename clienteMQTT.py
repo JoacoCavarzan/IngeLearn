9b295e98bc61f2c6008f0bffa545e1eca6aab53d
JoacoCavarzan <88760680+JoacoCavarzan@users.noreply.github.com>
2024-07-17 00:01:12 -0300

    Uniendo ambos protocolos

--- clienteMQTT.py
+++ clienteMQTT.py
@@ -1,5 +1,9 @@
 import paho.mqtt.client as mqtt
 import time
+from pymodbus.client import ModbusTcpClient
+
+PLC = ModbusTcpClient("192.168.5.20")
+PLC.conect()
 
 servidor = "broker.mqtt.cool"
 cliente = mqtt.Client(protocol=mqtt.MQTTv5)
@@ -7,6 +11,12 @@ cliente = mqtt.Client(protocol=mqtt.MQTTv5)
 cliente.connect(servidor, 1883)
 
 # --------------------------------------------------------------
-while True:
-    cliente.publish("DesafioI40","Mensaje desde Python")
-    time.sleep(1)
+try:
+    while True:
+        lectura = PLC.read_discrete_inputs(0, 1)
+        estadoEntrada = lectura.bita[0]
+        cliente.publish("EntradaPLC", estadoEntrada)
+        time.sleep(0.2)
+
+except:
+    PLC.close()
